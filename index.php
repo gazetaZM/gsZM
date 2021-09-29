6f081cef0c17d57bfebd585c88c1448d02714365
gazetaZM <91637946+gazetaZM@users.noreply.github.com>
2021-09-30 00:21:11 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
      if (lcfirst ($_POST['key'])=="herbata")
      {
        $haslo=1;
-       //header("Location: /2z5-gofry/");
+       //header("Location: /2z5-gofry");
      }else {
        header("Location: zle.php");
      }
